fc12396a38c00c4ee4aca0cc7a6941e9586c1130
castagnaTM <castagna.tm@gmail.com>
2019-09-24 15:49:26 -0400

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :first_name, uniqueness: { case_sensitive: false }
+    validates :title, uniqueness: { case_sensitive: false }
 end
